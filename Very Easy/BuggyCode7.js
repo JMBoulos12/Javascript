d0730806330d00a31dfde93cf34bc367b1e2acd2
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-20 10:25:59 +0200

    Create BuggyCode7.js

---
+++ Very Easy/BuggyCode7.js
@@ -0,0 +1,35 @@
+
+
+
+/*
+  Mubashir wants to swap two given numbers!
+
+  It is not returning the right values. Can you help him fix it?
+
+  a = 100
+  b = 200
+  a, b = swap(a, b)
+  print(a, b) // Should print out "200, 100", but the function prints out "100, 100"
+  
+  Examples  :
+  swap(100, 200) ➞ [200, 100]
+
+  swap(44, 33) ➞ [33, 44]
+
+  swap(21, 12) ➞ [12, 21]
+  
+  20-January-2023
+*/
+
+//First Method
+function swap(a, b){
+  t = a // Store value of `a` in a temporary variable `t`
+	a = b // Store value of `b` in `a`
+	b = t // Store value of `t` in `b`
+	return [a, b]
+}
+/* Second Method
+function swap(a, b){
+	return [b, a]
+}
+*/
