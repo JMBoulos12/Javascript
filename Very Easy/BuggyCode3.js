0f1aa56900dc3845b7082628fe44518c39ed0b54
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-20 10:13:58 +0200

    Create BuggyCode3.js

---
+++ Very Easy/BuggyCode3.js
@@ -0,0 +1,19 @@
+
+
+
+/*
+  Buggy Code (Part 3)
+  
+  Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+  Examples  :
+  sumArray([1, 2, 3, 4, 5]) ➞ 15
+
+  sumArray([-1, 0, 1]) ➞ 0
+
+  sumArray([0, 4, 8, 12]) ➞ 24
+  
+  20-January-2023
+*/
+
+let sumArray = a => a.reduce((a,b)=> a+b);
