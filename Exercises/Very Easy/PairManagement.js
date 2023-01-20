93efd91fa61298c11a7fb1dc62020213209ab768
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-20 14:05:37 +0200

    Create PairManagement.js

---
+++ Exercises/Very Easy/PairManagement.js
@@ -0,0 +1,17 @@
+
+
+
+
+/*
+  Given two arguments, return an array which contains these two arguments.
+  Examples  :
+  makePair(1, 2) ➞ [1, 2]
+  makePair(51, 21) ➞ [51, 21]
+  makePair(512124, 215) ➞ [512124, 215]
+  
+  20-January-2023
+*/
+
+function makePair(num1, num2) {
+  return [...arguments]
+}
