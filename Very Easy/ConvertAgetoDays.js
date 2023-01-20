9bb9c6f30d075730a81be63b7abd7fa3f5539176
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-20 10:33:48 +0200

    Create ConvertAgetoDays.js

---
+++ Very Easy/ConvertAgetoDays.js
@@ -0,0 +1,18 @@
+
+
+
+/*
+  Create a function that takes the age in years and returns the age in days.
+  Examples  :
+  calcAge(65) ➞ 23725
+  calcAge(0) ➞ 0
+  calcAge(20) ➞ 7300
+  
+  Notes :
+  Use 365 days as the length of a year for this challenge.
+  Ignore leap years and days between last birthday and now.
+  Expect only positive integer inputs.
+  20-January-2023
+*/
+
+const calcAge = age => age * 365;
