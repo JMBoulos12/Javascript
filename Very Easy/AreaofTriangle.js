216efadddf480fc65e5ad48d8a8628db96b0be97
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-20 10:07:13 +0200

    Create AreaofTriangle.js

---
+++ Very Easy/AreaofTriangle.js
@@ -0,0 +1,16 @@
+
+
+
+/*
+  Write a function that takes the base and height of a triangle and return its area.
+  Examples  :
+  triArea(3, 2) ➞ 3
+  triArea(7, 4) ➞ 14
+  triArea(10, 10) ➞ 50
+  
+  Notes :
+  The area of a triangle is: (base * height) / 2
+  20-January-2023
+*/
+
+const triArea = (base, height) => (base * height) / 2;
