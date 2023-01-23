cb57f74b09d64d46b6eff340eb8034c0fc6af824
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-23 10:34:32 +0200

    Create HextoBinary.js

---
+++ Exercises/Easy/HextoBinary.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+/*
+  Create a function that will take a HEX number and returns the binary equivalent (as a string).
+
+  Examples :
+  toBinary(0xFF) ➞ "11111111"
+
+  toBinary(0xAA) ➞ "10101010"
+
+  toBinary(0xFA) ➞ "11111010"
+  
+  Notes :
+  The number will be always an 8-bit number.
+
+  23-January-2023
+*/
+
+function toBinary(n){
+  return n.toString(2)
+}
