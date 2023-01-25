56f63ce1e9c56a228cb40cda1ed4d16d32176ead
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-25 11:59:53 +0200

    Create d.js

---
+++ Exercises/Medium/RegEx Exercises/Character Classes/VIII/d.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+/*
+  You can think of character classes as characters with special meaning. 
+  They are recognized as special when you place the \ before the character.
+
+  Here are a list of the characters classes in JavaScript:
+
+  ., \cX, \d, \D, \f, \n, \r, \s, \S, \t, \v, \w, \W, \0, \xhh, \uhhhh, \uhhhhh, [\b]
+
+  Extract the addresses from this string:
+
+  const str = "123 Redding Dr. 1560 Knoxville Ave. 3030 Norwalk Dr. 5 South St."
+  // ["123 Redding Dr.", "1560 Knoxville Ave.", "3030 Norwalk Dr.", "5 South St."]
+  
+  Write the regular expression that matches all street addresses. 
+  All street addresses begin with a number. Use the character class \d in your expression.
+
+  25-January-2023
+*/
+
+const REGEXP = /\d+.+?\./g
