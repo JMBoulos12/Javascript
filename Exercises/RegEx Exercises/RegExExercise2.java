651c0e4d19806486995466a4c6599ecc53c8f229
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-26 08:41:15 +0200

    Update RegExExercise2.java

--- Exercises/RegEx Exercises/RegExExercise2.java
+++ Exercises/RegEx Exercises/RegExExercise2.java
@@ -4,6 +4,7 @@
 
 
 /*
+  Find HTML Tags :
   Create a regular expression to find all (opening and closing) HTML tags with their attributes. Assume that tag attributes may not contain < and > (inside quotes too).
 
   Example :
