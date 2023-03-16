73879cf7fe3bb4b575b3b4c78dd7bc1eee88b0a5
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-16 15:50:05 +0200

    Create HelloWorld.js

---
+++ Problems/Leetcode/Programiz/1- Introduction/1- Print Hello World/3- Using document.write()/HelloWorld.js
@@ -0,0 +1,13 @@
+
+
+
+
+/*
+  document.write() is used when you want to print the content to the HTML document.
+  
+  16-March-2023
+*/
+
+
+// the hello world program
+document.write('Hello, World!');
