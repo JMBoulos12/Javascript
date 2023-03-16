5100cd5e27478d2630cc45f409d07b5e82d69ede
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-16 15:47:23 +0200

    Create HelloWorl.js

---
+++ Problems/Leetcode/Programiz/1- Introduction/1- Print Hello World/1- Using console.log()/HelloWorl.js
@@ -0,0 +1,23 @@
+
+
+
+
+/*
+  console.log() is used in debugging the code.
+  
+  Output  :
+  Hello, World!
+
+  Here, the first line is a comment.
+  // the hello world program
+  
+  The second line
+  console.log('Hello, World!');
+  
+  prints the 'Hello, World!' string to the console.
+
+  16-March-2023
+*/
+
+// the hello world program
+console.log('Hello World');
