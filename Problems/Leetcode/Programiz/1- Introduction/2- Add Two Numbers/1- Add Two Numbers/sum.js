e3c7e13408929aec0d9d718a4069e3adbba7342e
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-16 15:51:46 +0200

    Create sum.js

---
+++ Problems/Leetcode/Programiz/1- Introduction/2- Add Two Numbers/1- Add Two Numbers/sum.js
@@ -0,0 +1,19 @@
+
+
+
+
+/*
+  writeHere
+  
+  16-March-2023
+*/
+
+
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
