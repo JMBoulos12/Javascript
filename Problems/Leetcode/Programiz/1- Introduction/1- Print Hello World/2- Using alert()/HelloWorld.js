9e12bf2136fbe277df0abb0e897a46b9a9fbf35c
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-16 15:48:43 +0200

    Create HelloWorld.js

---
+++ Problems/Leetcode/Programiz/1- Introduction/1- Print Hello World/2- Using alert()/HelloWorld.js
@@ -0,0 +1,12 @@
+
+
+
+
+/*
+  The alert() method displays an alert box over the current window with the specified message.
+  
+  16-March-2023
+*/
+
+// the hello world program
+alert("Hello, World!");
