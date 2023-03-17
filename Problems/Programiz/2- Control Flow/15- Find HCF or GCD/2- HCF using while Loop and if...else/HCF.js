8463b2a6852be6b33cf8c8e5e7146f42261cf198
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-17 11:25:11 +0200

    Update HCF.js

--- Problems/Programiz/2- Control Flow/15- Find HCF or GCD/2- HCF using while Loop and if...else/HCF.js
+++ Problems/Programiz/2- Control Flow/15- Find HCF or GCD/2- HCF using while Loop and if...else/HCF.js
@@ -3,12 +3,17 @@
 
 
 /*
+  The Highest Common Factor (HCF) or Greatest Common Divisor (GCD) of two integers is the 
+  largest integer that can exactly divide both integers (without a remainder).
+
+  For example, the HCF of 60 and 72 is 12.
+
   Output  :
   Enter a first integer: 60
   Enter a second integer: 72
   HCF is 12
   
-  In the above program, a while loop is used with an if...else statement.
+  In this program, a while loop is used with an if...else statement.
 
   In each iteration, the smaller integer is subtracted from the larger integer. And the result is assigned to a variable holding the larger integer.
 
