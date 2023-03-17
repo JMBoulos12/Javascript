0eb05a697c871cbbb40c35dddc1910daffbb530e
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-17 09:51:33 +0200

    Create maxnumb.js

---
+++ Problems/Programiz/2- Control Flow/4- Find the Largest Among Three Numbers/2- Using Math.max()/maxnumb.js
@@ -0,0 +1,32 @@
+
+
+
+
+/*
+  You can also use the JavaScript built-in Math.max() function to find the largest among the numbers.
+  
+  Output  :
+  Enter first number: 5
+  Enter second number: 5.5
+  Enter third number: 5.6
+  The largest number is 5.6
+  
+  Math.max() returns the largest number among the provided numbers.
+
+  You can use Math.min() function to find the smallest among the numbers.
+
+  17-March-2023
+*/
+
+
+// program to find the largest among three numbers
+
+// take input from the user
+const num1 = parseFloat(prompt("Enter first number: "));
+const num2 = parseFloat(prompt("Enter second number: "));
+const num3 = parseFloat(prompt("Enter third number: "));
+
+const largest = Math.max(num1, num2, num3);
+
+// display the result
+console.log("The largest number is " + largest);
