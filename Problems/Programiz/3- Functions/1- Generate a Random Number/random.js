24a1e338738721b24c6f13887e631588930fb58d
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-17 12:01:39 +0200

    Create random.js

---
+++ Problems/Programiz/3- Functions/1- Generate a Random Number/random.js
@@ -0,0 +1,22 @@
+
+
+
+
+/*
+  In JavaScript, you can generate a random number with the Math.random() function.
+
+  Math.random() returns a random floating-point number ranging from 0 to less than 1 (inclusive of 0 and exclusive of 1)
+  
+  Output  :
+  0.5856407221615856
+  Here, we have declared a variable a and assigned it a random number greater than or equal to 0 and less than 1.
+
+  Note: You might get a different output in the above program as Math.random() will generate a random number.
+
+  17-March-2023
+*/
+
+
+// generating  a random number
+const a = Math.random();
+console.log(a);
