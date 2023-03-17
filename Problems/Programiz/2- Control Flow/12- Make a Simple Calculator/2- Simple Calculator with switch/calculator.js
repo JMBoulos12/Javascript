5df8050bad1c15f94317e6195b3900a1a49358d0
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-17 10:42:48 +0200

    Create calculator.js

---
+++ Problems/Programiz/2- Control Flow/12- Make a Simple Calculator/2- Simple Calculator with switch/calculator.js
@@ -0,0 +1,53 @@
+
+
+
+
+/*
+  Output  :
+  Enter operator: +
+  Enter first number: 4
+  Enter second number: 5
+  4 + 5 = 9
+  
+  In above program, the user is asked to enter either +, -, * or /, and two numbers. 
+  Then, the switch statement executes cases based on the user input.
+
+  17-March-2023
+*/
+
+
+// program for a simple calculator
+let result;
+
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
