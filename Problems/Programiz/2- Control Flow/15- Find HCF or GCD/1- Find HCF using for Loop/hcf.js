4d94df2f3f5cd5e875f3a44f91eab7039060ee49
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-17 11:25:54 +0200

    Update hcf.js

--- Problems/Programiz/2- Control Flow/15- Find HCF or GCD/1- Find HCF using for Loop/hcf.js
+++ Problems/Programiz/2- Control Flow/15- Find HCF or GCD/1- Find HCF using for Loop/hcf.js
@@ -3,6 +3,11 @@
 
 
 /*
+  The Highest Common Factor (HCF) or Greatest Common Divisor (GCD) of two integers 
+  is the largest integer that can exactly divide both integers (without a remainder).
+
+  For example, the HCF of 60 and 72 is 12.
+
   Output  :
   Enter a first integer: 60
   Enter a second integer: 72
