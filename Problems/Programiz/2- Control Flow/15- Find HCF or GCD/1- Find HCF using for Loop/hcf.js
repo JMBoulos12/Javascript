abc0d70c480fbedd83e5a342318af823a8572179
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-17 11:22:43 +0200

    Create hcf.js

---
+++ Problems/Programiz/2- Control Flow/15- Find HCF or GCD/1- Find HCF using for Loop/hcf.js
@@ -0,0 +1,40 @@
+
+
+
+
+/*
+  Output  :
+  Enter a first integer: 60
+  Enter a second integer: 72
+  HCF of 60 and 72 is 12.
+  
+  In the above program, the user is prompted to enter two positive numbers.
+
+  The for loop is used to iterate from 1 to numbers entered by the user.
+
+  The if condition and modulus operator % is used to find the HCF of both numbers.
+
+  In the above condition, if both the integers number1 and number2 are exactly divisible by i, the highest integer value that fulfils that condition is calculated.
+
+  17-March-2023
+*/
+
+
+// program to find the HCF or GCD of two integers
+
+let hcf;
+// take input
+const number1 = prompt('Enter a first positive integer: ');
+const number2 = prompt('Enter a second positive integer: ');
+
+// looping from 1 to number1 and number2
+for (let i = 1; i <= number1 && i <= number2; i++) {
+
+    // check if is factor of both integers
+    if( number1 % i == 0 && number2 % i == 0) {
+        hcf = i;
+    }
+}
+
+// display the hcf
+console.log(`HCF of ${number1} and ${number2} is ${hcf}.`);
