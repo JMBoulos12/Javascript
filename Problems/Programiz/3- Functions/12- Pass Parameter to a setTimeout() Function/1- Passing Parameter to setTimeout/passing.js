3f676499c82006d8ec228b23d42bdae23579881e
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-18 22:15:47 +0200

    Create passing.js

---
+++ Problems/Programiz/3- Functions/12- Pass Parameter to a setTimeout() Function/1- Passing Parameter to setTimeout/passing.js
@@ -0,0 +1,20 @@
+
+
+
+
+/*
+  Write 
+  
+  18-March-2023
+*/
+
+
+// program to pass parameter to a setTimeout() function
+
+function greet() {
+    console.log('Hello world');
+}
+
+// passing parameter
+setTimeout(greet, 3000);
+console.log('This message is shown first');
