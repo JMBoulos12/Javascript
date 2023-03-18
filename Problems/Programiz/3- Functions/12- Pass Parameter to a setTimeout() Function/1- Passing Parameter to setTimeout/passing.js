d0b397d7bc82689db9f678fb0ea4861387ecedf2
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-18 22:17:42 +0200

    Update passing.js

--- Problems/Programiz/3- Functions/12- Pass Parameter to a setTimeout() Function/1- Passing Parameter to setTimeout/passing.js
+++ Problems/Programiz/3- Functions/12- Pass Parameter to a setTimeout() Function/1- Passing Parameter to setTimeout/passing.js
@@ -3,8 +3,25 @@
 
 
 /*
-  Write 
+  The setTimeout() method executes a block of code after the specified time. The method executes the code only once.
+
+  The commonly used syntax of JavaScript setTimeout is:
+  setTimeout(function, milliseconds);
   
+  Its parameters are:
+  * function - a function containing a block of code
+  * milliseconds - the time after which the function is executed 
+  
+  Output  :
+  This message is shown first
+  Hello world
+  
+  In the above program, the greet() function is passed to the setTimeout().
+
+  The greet() function then gets called after 3000 milliseconds (3 seconds).
+
+  Hence, the program displays the text Hello world only once after 3 seconds.
+
   18-March-2023
 */
 
