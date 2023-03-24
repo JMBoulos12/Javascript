da46627b35e4208d2ee69a84c786668eb7c8049c
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-24 09:19:07 +0200

    Create strings.js

---
+++ Problems/Programiz/5- Strings/10- Create Multiline Strings/2- Using \/strings.js
@@ -0,0 +1,24 @@
+
+
+
+
+/*
+  Output  :
+  This is a long message
+  that spans across multiple lines
+  in the code.
+  
+  In this example, a multiline string is created using \. \n is used to break the line.
+
+  24-March-2023
+*/
+
+
+// program to create a multiline strings
+
+// using the \ operator
+const message = 'This is a long message\n \
+that spans across multiple lines\n \
+in the code.'
+
+console.log(message);
