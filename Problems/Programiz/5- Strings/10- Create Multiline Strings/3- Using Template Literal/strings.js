406a946bf5d60d3506d296cbc54115e41649ad0f
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-24 09:20:32 +0200

    Create strings.js

---
+++ Problems/Programiz/5- Strings/10- Create Multiline Strings/3- Using Template Literal/strings.js
@@ -0,0 +1,27 @@
+
+
+
+
+/*
+  Output  :
+  This is a long message
+  that spans across multiple lines
+  in the code.
+  
+  In this example, the template literal ` ` is used to write multiline strings.
+
+  The template literal was introduced in the newer version of JavaScript (ES6).
+
+  24-March-2023
+*/
+
+
+// program to create a multiline strings
+
+// using the template literal
+
+const message = `This is a long message
+that spans across multiple lines
+in the code.`
+
+console.log(message);
