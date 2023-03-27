593d71aab3e6eef19d3441ad6c2cc3bc1859c6aa
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-27 11:20:17 +0300

    Create url.js

---
+++ Problems/Programiz/6- Miscellaneous/9- Get The Current URL/url.js
@@ -0,0 +1,25 @@
+
+
+
+
+/*
+  In this example, you will learn to write a JavaScript program that will get the current URL.
+  
+  Output  :
+  https://www.google.com/
+  https://www.google.com/
+  
+  In the above program, window.location.href property and document.URL property are used to get the URL of the current page.
+
+  Both the window.location.href and the document.URL properties return the URL of the current page.
+
+  27-March-2023
+*/
+
+
+// program to get the URL
+
+const url1 = window.location.href;
+const url2 = document.URL;
+console.log(url1);
+console.log(url2);
