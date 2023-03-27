10183ed0bb68ed3c92085913a05ba78fd9309e53
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-27 09:27:39 +0300

    Create string.js

---
+++ Problems/Programiz/5- Strings/13- Check if a String Starts With Another String/1- Using startsWith()/string.js
@@ -0,0 +1,31 @@
+
+
+
+
+/*
+  In this example, you will learn to write a JavaScript program that will check if a string starts with another string.
+  
+  Output  :
+  
+  The string starts with "he".
+  In the above program, the startsWith() method is used to determine if the string starts with 'he'. 
+  The startsWith() method checks if the string starts with the particular string.
+
+  The if...else statement is used to check the condition.
+
+  27-March-2023
+*/
+
+
+// program to check if a string starts with another string
+
+const string = 'hello world';
+
+const toCheckString = 'he';
+
+if(string.startsWith(toCheckString)) {
+   console.warn('The string starts with "he".');
+  }
+  else {
+      console.warn(`The string does not starts with "he".`);
+}
