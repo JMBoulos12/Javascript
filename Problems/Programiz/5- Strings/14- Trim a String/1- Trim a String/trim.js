678fbab58266302fe972e1cde746f33981783220
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-27 09:34:16 +0300

    Create trim.js

---
+++ Problems/Programiz/5- Strings/14- Trim a String/1- Trim a String/trim.js
@@ -0,0 +1,25 @@
+
+
+
+
+/*
+  In this example, you will learn to write a JavaScript program that will trim a string.
+  
+  Output  :
+  Hello World
+  
+  In this example, the trim() method is used to trim a string.
+
+  The trim() method removes white space from both sides of the string.
+
+  27-March-2023
+*/
+
+
+// program to trim a string
+
+const string = '      Hello World       ';
+
+const result = string.trim();
+
+console.log(result);
