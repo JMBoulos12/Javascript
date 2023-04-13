9045ec92a6a7aeffc0e66e53928262b9a576cfd9
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-04-13 10:21:18 +0300

    Create two-fer.js

---
+++ Problems/Exercism/26- Two Fer/two-fer.js
@@ -0,0 +1,37 @@
+
+
+
+
+/**
+  #Introduction:
+  In some English accents, when you say "two for" quickly, it sounds like "two fer". Two-for-one is a way of saying that if you buy one, you also get one for free. 
+  So the phrase "two-fer" often implies a two-for-one offer.
+
+  Imagine a bakery that has a holiday offer where you can buy two cookies for the price of one ("two-fer one!"). 
+  You go for the offer and (very generously) decide to give the extra cookie to a friend.
+
+  #Instructions:
+  Your task is to determine what you will say as you give away the extra cookie.
+
+  If your friend likes cookies, and is named Do-yun, then you will say:
+
+  One for Do-yun, one for me.
+  
+  If your friend doesn't like cookies, you give the cookie to the next person in line at the bakery. 
+  Since you don't know their name, you will say you instead.
+
+  One for you, one for me.
+  
+  Here are some examples:
+
+  Name	    Dialogue
+  Alice	    One for Alice, one for me.
+  Bohdan	  One for Bohdan, one for me.
+            One for you, one for me.
+  Zaphod	  One for Zaphod, one for me.
+  
+  13-April-2023
+*/
+
+
+export const twoFer = (name = "you") => `One for ${name}, one for me.`;
