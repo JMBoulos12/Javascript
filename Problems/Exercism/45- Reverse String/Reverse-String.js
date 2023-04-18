d78ad39ccfd3ba455e2a309da9ea9fd17c9b6372
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-04-18 08:50:55 +0300

    Create Reverse-String.js

---
+++ Problems/Exercism/45- Reverse String/Reverse-String.js
@@ -0,0 +1,15 @@
+
+
+
+
+/**
+  #Instructions
+  Reverse a string
+
+  For example: input: "cool" output: "looc"
+  
+  18-April-2023
+*/
+
+
+export const reverseString = inputString => [...inputString].reverse().join('');
