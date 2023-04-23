58c3a709834bb896d2a923de87fa9ba83fdd8218
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-04-23 12:01:32 +0300

    Create matching-brackets.js

---
+++ Problems/Exercism/70- Matching Brackets/matching-brackets.js
@@ -0,0 +1,34 @@
+
+
+
+
+/**
+  #Instructions
+  Given a string containing brackets [], braces {}, parentheses (), or any combination thereof, verify that any and all pairs are matched and nested correctly. 
+   
+  23-April-2023
+*/
+
+
+const BRACKET_MAP = {
+  '(': ')',
+  '[': ']',
+  '{': '}'
+};
+// Helper functions for clarity
+const bracketCharacters = string => string.match(/[([{}\])]/g) || [];
+const isOpenBracket = bracket => !!BRACKET_MAP[bracket];
+const arePairedBrackets = (openBracket, closingBracket) => BRACKET_MAP[openBracket] === closingBracket;
+const isEmptyStack = stack => stack.length === 0;
+/**
+ * I solved this problem using a stack. We iterate through all the bracket characters and, if we find an open bracket,
+ * we add it to the top of the stack. If we find a closing bracket, we take the open bracket on top of the stack and
+ * compare them to make sure they are pairs. We also check to make sure no open brackets are left on the stack after all
+ * of the comparisons are done as those don't have matching closing brackets.
+ */
+export const isPaired = string => {
+  const openBracketStack = [];
+  return bracketCharacters(string).every(bracket =>
+    isOpenBracket(bracket) ? openBracketStack.push(bracket) : arePairedBrackets(openBracketStack.pop(), bracket)
+  ) && isEmptyStack(openBracketStack);
+};
