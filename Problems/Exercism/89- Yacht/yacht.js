34e6d9779ce988a3da081dbd7c3cb398bf5e2983
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-04-28 12:24:43 +0300

    Create yacht.js

---
+++ Problems/Exercism/89- Yacht/yacht.js
@@ -0,0 +1,57 @@
+
+
+
+/**
+  Instructions
+  The dice game Yacht is from the same family as Poker Dice, Generala and particularly Yahtzee, of which it is a precursor. 
+  In the game, five dice are rolled and the result can be entered in any of twelve categories. The score of a throw of the dice depends on category chosen.
+
+  Scores in Yacht
+  Category	Score	Description	Example
+  Ones	1 × number of ones	Any combination	1 1 1 4 5 scores 3
+  Twos	2 × number of twos	Any combination	2 2 3 4 5 scores 4
+  Threes	3 × number of threes	Any combination	3 3 3 3 3 scores 15
+  Fours	4 × number of fours	Any combination	1 2 3 3 5 scores 0
+  Fives	5 × number of fives	Any combination	5 1 5 2 5 scores 15
+  Sixes	6 × number of sixes	Any combination	2 3 4 5 6 scores 6
+  Full House	Total of the dice	Three of one number and two of another	3 3 3 5 5 scores 19
+  Four of a Kind	Total of the four dice	At least four dice showing the same face	4 4 4 4 6 scores 16
+  Little Straight	30 points	1-2-3-4-5	1 2 3 4 5 scores 30
+  Big Straight	30 points	2-3-4-5-6	2 3 4 5 6 scores 30
+  Choice	Sum of the dice	Any combination	2 3 3 4 6 scores 18
+  Yacht	50 points	All five dice showing the same face	4 4 4 4 4 scores 50
+  
+  If the dice do not satisfy the requirements of a category, the score is zero. If, for example, Four Of A Kind is entered in the Yacht category, zero points are scored. A Yacht scores zero if entered in the Full House category.
+
+  Task
+  Given a list of values for five dice and a category, your solution should return the score of the dice for that category. If the dice do not satisfy the requirements of the category your solution should return 0. You can assume that five values will always be presented, and the value of each will be between one and six inclusively. You should not assume that the dice are ordered.
+  
+  28-April-2023
+*/
+
+
+const sum = array => array.reduce((sum, num) => sum + num, 0);
+const sumRoll = target => dice => sum(dice.filter(roll => roll === target));
+const isStraight = dice => [...dice]
+  .sort((a, b) => a - b)
+  .every((roll, i, src) => !i || roll === src[i - 1] + 1);
+const getKind = dice => (dice.lastIndexOf(dice[0]) !== 0 ? dice[0] : dice[1]);
+const isFourOfKind = dice => {
+  const kind = getKind(dice);
+  return dice.reduce((count, roll) => (roll === kind ? count + 1 : count), 0) >= 4;
+};
+const CATEGORIES = {
+  yacht: dice => +(dice.every((roll, _, src) => roll === src[0]) && 50),
+  choice: sum,
+  'big straight': dice => +(isStraight(dice) && dice.includes(6) && 30),
+  'little straight': dice => +(isStraight(dice) && dice.includes(1) && 30),
+  'four of a kind': dice => +(isFourOfKind(dice) && getKind(dice) * 4),
+  'full house': dice => +(new Set(dice).size === 2 && !isFourOfKind(dice) && sum(dice)),
+  sixes: sumRoll(6),
+  fives: sumRoll(5),
+  fours: sumRoll(4),
+  threes: sumRoll(3),
+  twos: sumRoll(2),
+  ones: sumRoll(1)
+};
+export const score = (dice, category) => CATEGORIES[category](dice);
